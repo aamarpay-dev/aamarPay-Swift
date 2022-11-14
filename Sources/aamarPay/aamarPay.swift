a4e683384f2a75b107c6b50d2d3a7dc7a9f64c09
sanjidbillah <raiyansafir@gmail.com>
2022-11-14 16:42:24 +0600

    Converter added

--- Sources/aamarPay/aamarPay.swift
+++ Sources/aamarPay/aamarPay.swift
@@ -161,3 +161,12 @@ extension CharacterSet {
     return allowed
   }()
 }
+
+extension String {
+    func convertToDictionary() -> [String: Any]? {
+        if let data = data(using: .utf8) {
+            return try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+        }
+        return nil
+    }
+}
