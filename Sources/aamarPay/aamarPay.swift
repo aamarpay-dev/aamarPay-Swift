fe9a7e8e5b37e8c3a87af86e332ebbfeeb4e90bd
sanjidbillah <raiyansafir@gmail.com>
2022-11-15 13:14:34 +0600

    Make modal fullscreen

--- Sources/aamarPay/aamarPay.swift
+++ Sources/aamarPay/aamarPay.swift
@@ -97,6 +97,7 @@ open class aamarPay: UIViewController {
               aamarPay.screen!.webView = WKWebView()
               let paymentFrontController = UINavigationController.init(rootViewController: aamarPay.screen!)
               aamarPay.screen!.paymentCompletation = completion
+              parent.modalPresentationStyle = .fullScreen
               parent.present(paymentFrontController, animated: true, completion: nil)
           }
         }
